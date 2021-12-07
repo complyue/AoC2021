655a2897c19d7caa046bffa1ec4de7b383b52f72
ComplYue <compl.yue@icloud.com>
2021-12-07 14:21:10 +0800

    reuse a bit

--- day7/solution.hs
+++ day7/solution.hs
@@ -5,25 +5,8 @@ import Control.Arrow
 import Data.List
 
 -- %{
-part1 :: [(Int, Int)] -> Int
-part1 pgs = minimum [fuel p pgs | p <- [p'min .. p'max]]
-  where
-    (p'min, p'max) =
-      let (p'min, _) = head pgs
-          (p'max, _) = last pgs
-       in (p'min, p'max)
-    fuel :: Int -> [(Int, Int)] -> Int
-    fuel p = go 0
-      where
-        go :: Int -> [(Int, Int)] -> Int
-        go cum [] = cum
-        go cum ((op, cnt) : rest) = go (cum + abs (p - op) * cnt) rest
-
--- %}
-
--- %{
-part2 :: [(Int, Int)] -> Int
-part2 pgs = minimum [fuel p pgs | p <- [p'min .. p'max]]
+minFuel :: [(Int, Int)] -> (Int -> Int) -> Int
+minFuel pgs formula = minimum [fuel p pgs | p <- [p'min .. p'max]]
   where
     (p'min, p'max) =
       let (p'min, _) = head pgs
@@ -35,9 +18,7 @@ part2 pgs = minimum [fuel p pgs | p <- [p'min .. p'max]]
         go :: Int -> [(Int, Int)] -> Int
         go cum [] = cum
         go cum ((op, cnt) : rest) =
-          go (cum + (nsteps * (nsteps + 1) `div` 2) * cnt) rest
-          where
-            nsteps = abs $ p - op
+          go (cum + formula (abs (p - op)) * cnt) rest
 
 -- %}
 
@@ -55,7 +36,7 @@ main = do
   -- %}
 
   -- %% -- Part 1
-  print $ part1 pgs
+  print $ minFuel pgs id
 
   -- %% -- Part 2
-  print $ part2 pgs
+  print $ minFuel pgs $ \nsteps -> nsteps * (nsteps + 1) `div` 2
